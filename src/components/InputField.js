30c587e7251e9d4ca60bf7067cb1b6b6b8fcf599
david-aihe <doa9200@gmail.com>
2022-05-15 19:10:08 -0400

    Input field local state and handlers in place

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -4,14 +4,25 @@ import TextareaAutosize from 'react-textarea-autosize';
 
 const InputField = () => {
     const [prompt, setPrompt] = useState('');
-    
+
+    const handleChange = (e) => {
+        setPrompt(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //Send prompt axios call and push prompt to conversation state
+        console.log(prompt);
+        setPrompt('')
+    }
+
     return (
         <section className="absolute bottom-4 flex items-center justify-between w-full p-3 bg-black ">
             <TextareaAutosize maxRows={3} type="textarea" placeholder="Enter a Prompt"
             className="block w-full py-2 pl-4 mx-3 bg-transparent rounded-xl border-2 border-gray-500 opacity-100 text-white"
-            name="message" />
+            name="prompt" value={prompt} onChange={handleChange}/>
             
-            <button>
+            <button onClick={handleSubmit}>
                 <BsFillArrowUpCircleFill className='w-7 h-7 text-blue-500'/>
             </button>
         </section>
